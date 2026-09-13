a10db30590b9d46068f5feabb2ab5c3c73e14165
AriesHere <234637552+AriesHere@users.noreply.github.com>
2026-09-13 22:22:39 +0800

    temp

--- GDUTSharp/Interfaces/INoticeService.cs
+++ GDUTSharp/Interfaces/INoticeService.cs
@@ -3,6 +3,9 @@
 /// <summary>
 /// 通知简讯网
 /// </summary>
+/// <remarks>
+/// 首次使用时需要先调用 <see cref="Preprocess"/> 才能再调用其它的
+/// </remarks>
 public interface INoticeService
 {
     /// <summary>预处理</summary>
@@ -10,20 +13,20 @@ public interface INoticeService
     /// 由于通知公文网的特殊性，无法单独获取下面这四种信息，
     /// 必须先进行预处理，同时获得它们四个的链接
     /// </remarks>
-    public void Preprocess();
+    public Task<bool> Preprocess();
 
-    /// <summary>最新通知，仅校园网环境可用</summary>
+    /// <summary>最新通知</summary>
     /// <remarks>大部分类似于“关于xxx的通知”</remarks>
     public void GetNotice();
 
-    /// <summary>最新简讯，仅校园网环境可用</summary>
+    /// <summary>最新简讯</summary>
     /// <remarks>大部分是宣传性文章</remarks>
     public void GetBulletin();
 
-    /// <summary>最新公告，仅校园网环境可用</summary>
+    /// <summary>最新公告</summary>
     /// <remarks>大部分是“xxx情况公布”或“xxx公示”</remarks>
     public void GetAnnouncement();
 
-    /// <summary>招标公告，非校园网环境也可用</summary>
+    /// <summary>招标公告</summary>
     public void GetTender();
 }
--- GDUTSharp/Services/NoticeService.cs
+++ GDUTSharp/Services/NoticeService.cs
@@ -1,4 +1,5 @@
 ﻿using GDUTSharp.Interfaces;
+using GDUTSharp.Shared;
 using Microsoft.Extensions.Logging;
 
 namespace GDUTSharp.Services;
@@ -8,9 +9,51 @@ public class NoticeService(ILogger<NoticeService> logger, ICommonClient client) 
     protected ILogger<NoticeService> _logger = logger;
     protected ICommonClient _client = client;
 
-    public async virtual void Preprocess()
+    public async virtual Task<bool> Preprocess()
     {
-        throw new NotImplementedException();
+        HttpRequestMessage? request = null;
+        HttpResponseMessage? response = null;
+        try
+        {
+            request = new(HttpMethod.Get, GDUTConstant.NOTICE_BEFORE_LOGIN);
+            response = await _client.SendAsync(request);
+            request.Dispose();
+            var r = await response.Content.ReadAsStringAsync();
+            response.Dispose();
+            var position = r.IndexOf('?');
+            var url = r[position..r.IndexOf('\"', position)];
+
+            request = new(HttpMethod.Get, GDUTConstant.NOTICE_BEFORE_LOGIN + url);
+            response = await _client.SendAsync(request);
+            request.Dispose();
+            r = await response.Content.ReadAsStringAsync(); // 返回内容很大
+            var p1 = r.IndexOf("公文悬浮菜单js");
+            var span = r.AsSpan(p1);
+            position = span.IndexOf("/seeyon/main.do?");
+            url = span[position..span.IndexOf('\"')].ToString();
+
+            request = new(HttpMethod.Get, url);
+            response = await _client.SendAsync(request);
+            request.Dispose();
+            r = await response.Content.ReadAsStringAsync();
+
+            // (0,0) => 图片横幅
+            // (0,1) => 公告查询
+            // (1,1) => 招标公告
+            // (0,2) => 最新通知
+            // (0,3) => 最新简讯
+            // (1,3) => 最新公告
+        }
+        catch (Exception e)
+        {
+            if (_logger.IsEnabled(LogLevel.Error)) _logger.LogError("认证或登录异常。 {Exception}", e);
+            return false;
+        }
+        finally
+        {
+            request?.Dispose();
+            response?.Dispose();
+        }
     }
 
     public async virtual void GetAnnouncement()
